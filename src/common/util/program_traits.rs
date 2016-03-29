f5682ef613b92acb179feb0bd876156557b44667
Louis Capitanchik <ljcapitanio@gmail.com>
2016-03-29 01:28:30 +0100

    Added comment to ConfigurableProgram advising Panic for non-recoverable config error

--- src/common/util/program_traits.rs
+++ src/common/util/program_traits.rs
@@ -6,5 +6,5 @@ pub trait ProgramFragment {
 pub trait ConfigurableProgram<T:ProgramFragment> {
     fn set_flag(&mut self, key:String, value:bool) -> &mut Self;
     fn set_arg(&mut self, key:String, value:String) -> &mut Self;
-    fn config(&self) -> T;
+    fn config(&self) -> T; // Should panic on incorrectly defined Programs, User cannot recover
 }
