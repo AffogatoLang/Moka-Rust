b92274976557be38ddeee1c2b35762e5cc15d6fe
Louis Capitanchik <ljcapitanio@gmail.com>
2016-04-11 22:43:38 +0100

    Implemented series of funcs for creating syntax error messages

--- src/common/util/mod.rs
+++ src/common/util/mod.rs
@@ -1,8 +1,14 @@
 mod program_traits;
 mod file_traits;
 mod file_util;
+mod string_util;
 
 pub use self::program_traits::ProgramFragment;
 pub use self::program_traits::ConfigurableProgram;
 pub use self::file_traits::Loader;
 pub use self::file_util::get_bin_dir;
+pub use self::string_util::get_line_col;
+pub use self::string_util::generate_syntax_error;
+pub use self::string_util::generate_syntax_error_with_slug;
+pub use self::string_util::generate_syntax_error_from_index;
+pub use self::string_util::generate_syntax_error_with_slug_from_index;
---
+++ src/common/util/string_util.rs
@@ -0,0 +1,52 @@
+pub fn get_line_col(src: &String, index: usize) -> (usize, usize) {
+    let slices = src.split_at(index);
+    let prev:&str = slices.0;
+
+    let mut cur_index = 0;
+
+    let mut line = 0;
+    let mut index_of_line = 0;
+
+    for ch in prev.chars() {
+        match ch {
+            '\n' => {line = line + 1; index_of_line = cur_index;},
+            _ => ()
+        }
+        cur_index = cur_index + 1;
+    }
+
+    let column = index - index_of_line;
+
+    (line+1, column+1) // Line and column aren't typically 0 based
+}
+
+pub fn generate_syntax_error(src : &String, location : (usize, usize)) -> String {
+    generate_syntax_error_with_slug(src, location, "")
+}
+
+pub fn generate_syntax_error_with_slug<'a>(src: &String, location: (usize, usize), slug: &'a str) -> String {
+    let mut out : String = "Syntax Error at [".to_string();
+    out.push_str(&(location.0.to_string()));
+    out.push_str(", ");
+    out.push_str(&(location.1.to_string()));
+    out.push_str("] : ");
+
+    let prelength = out.len();
+
+    out.push_str(src.lines().skip(location.0 - 1).next().unwrap());
+
+    // Location.1 is column, which is not 0 based for display but should be for spacing
+    let mut pointing : String = String::from_utf8(vec![b' ';prelength + location.1 - 1]).unwrap();
+    pointing.push_str("^~~~ ");
+    pointing.push_str(slug);
+    out.push_str("\n");
+    out + &pointing
+}
+
+pub fn generate_syntax_error_with_slug_from_index<'a>(src: &String, index: usize, slug: &'a str) -> String {
+    generate_syntax_error_with_slug(src, get_line_col(src, index), slug)
+}
+
+pub fn generate_syntax_error_from_index(src: &String, index: usize) -> String {
+    generate_syntax_error(src, get_line_col(src, index))
+}
